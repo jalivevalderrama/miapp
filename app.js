e2cafc451f26dd4c0dca344c441bea8dfcf31588
Alejandro9088 <alejandrofuentes@insucodos.cl>
2025-07-15 13:21:27 -0400

    boton deeliminar funcional

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const formulario= document.querySelector(".form-create");
+let cont = 1;
 formulario.addEventListener("submit", (e)=>{
     e.preventDefault();
     const descripcion = document.querySelector("#descripcion").value;
     const contenedor = document.querySelector(".publicaciones");
-    contenedor.innerHTML += `<div class="publicacion">
-                <p>id: 00</p>
-                <p>${descripcion}</p>
-                <button>editar</button>
-                <button>eliminar</button>
-            </div>`;
+    contenedor.innerHTML += `<div id="${cont}" class="publicacion">
+            <p>id:${cont}</p>
+            <p>${descripcion}</p>
+            <button id="editar" onclick="editar"(this)>editar</button>
+            <button id="eliminar" onclick="eliminar"(this)>eliminar</button>
+        </div>`;
+    cont++;
 });
+const eliminar = document.querySelector("#eliminar");
+eliminar.adddEventListener("click", (e)=>{
+    console.log("se presiono click en el boton eliminar")
+    });
+function editar(e){
+
+}
+function eliminar(e){
+    e.parentElement.remove();
+}
